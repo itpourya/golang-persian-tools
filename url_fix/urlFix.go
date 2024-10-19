d9269077bd1cbc1df0fb26b3e4f1f42f7100da60
sandipsky <sandipshakya75@gmail.com>
2024-10-19 07:55:04 +0330

    Add: add url validator and fix

---
+++ url_fix/urlFix.go
@@ -0,0 +1,21 @@
+package urlfix
+
+import (
+	"net/url"
+	"strings"
+)
+
+func urlFix(link string, separator string) string {
+	// Decode the URL
+	decodedURL, err := url.QueryUnescape(string(link))
+	if err != nil {
+		return ""
+	}
+
+	// If a separator is provided, replace spaces with the separator
+	if separator != "" {
+		return strings.ReplaceAll(decodedURL, " ", string(separator))
+	}
+
+	return decodedURL
+}
